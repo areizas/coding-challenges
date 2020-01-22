bf0749172fd9c3bb8fc9240a54039aaf074987b1
mateo.areiza <mateo.areiza@globant.com>
2020-01-22 09:50:55 -0500

    Create function perform binary operation

--- javascript/binary-calculator/js/binaryCalculator.js
+++ javascript/binary-calculator/js/binaryCalculator.js
@@ -2,24 +2,53 @@ let binaryEquation = '';
 let operationPressed = false;
 
 const clickOnNumber = (btn) => {
-    let response = document.getElementById("res");
+    let result = document.getElementById("res");
     binaryEquation+=btn.target.getAttribute('value');
-    response.innerHTML = binaryEquation;
+    result.innerHTML = binaryEquation;
 };
 
 const clickOnOperation = (btn) => {
     if(! operationPressed){
-        let response = document.getElementById("res");
+        let result = document.getElementById("res");
         binaryEquation+=btn.target.getAttribute('value');
-        response.innerHTML = binaryEquation;
+        result.innerHTML = binaryEquation;
         operationPressed = true
     }
 };
 
 const clickOnClear = () => {
-    let response = document.getElementById("res");
+    let result = document.getElementById("res");
     binaryEquation="";
-    response.innerHTML = binaryEquation;
+    result.innerHTML = binaryEquation;
+    operationPressed = false;
+};
+
+const clickOnEql = () => {
+    let result = document.getElementById("res");
+    let values = [];
+
+    if(binaryEquation.includes("+")){
+        values = binaryEquation.split('+');
+        let num1 = parseInt(values[0],2);
+        let num2 = parseInt(values[1],2);
+        result.innerHTML = (num1+num2).toString(2);
+    } else if(binaryEquation.includes("-")){
+        values = binaryEquation.split('-');
+        let num1 = parseInt(values[0],2);
+        let num2 = parseInt(values[1],2);
+        result.innerHTML = (num1-num2).toString(2);
+    } else if(binaryEquation.includes("*")){
+        values = binaryEquation.split('*');
+        let num1 = parseInt(values[0],2);
+        let num2 = parseInt(values[1],2);
+        result.innerHTML = (num1*num2).toString(2);
+    } else if(binaryEquation.includes("/")){
+        values = binaryEquation.split('/');
+        let num1 = parseInt(values[0],2);
+        let num2 = parseInt(values[1],2);
+        result.innerHTML = (Math.floor(num1/num2)).toString(2);
+    }
+
 };
 
 const setButtonFunction = () =>{
@@ -30,6 +59,7 @@ const setButtonFunction = () =>{
     document.getElementById("btnMul").onclick = clickOnOperation;
     document.getElementById("btnDiv").onclick = clickOnOperation;
     document.getElementById("btnClr").onclick = clickOnClear;
+    document.getElementById("btnEql").onclick = clickOnEql;
 };
 
 setButtonFunction();
